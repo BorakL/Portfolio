279286a00e11f74ed31df24988b89530a4f58de9
BorakL <lborak88@gmail.com>
2023-03-20 00:50:52 +0100

    changed href for portfolio

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
             <li><a onClick={()=>setSidebarOpen(false)} href="#contact">Contact</a></li> 
             <li>
               <a onClick={()=>setSidebarOpen(false)} 
-                  href="borakl.github.io/Portfolio/blob/master/public/Resume.pdf"
+                  href="Portfolio/Resume.pdf"
                   target="_blank">Resume
               </a>
             </li>
